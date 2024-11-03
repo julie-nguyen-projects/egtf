43cf2ef5730b4d65ca0a585f072b143cd55074dd
Julie NGUYEN <julie_nguyen@live.fr>
2024-11-03 10:37:39 +0100

    (feat) feature liée aux élèves

---
+++ src/controllers/eleveController.js
@@ -0,0 +1,25 @@
+const EleveService = require("../services/eleveService");
+
+const EleveController = {
+    findAll: () => {
+        return EleveService.findALl();
+    },
+
+    findById: (id) => {
+        return EleveService.findById(id);
+    },
+
+    create: (eleve) => {
+        return EleveService.create(eleve);
+    },
+
+    update: (eleve, id) => {
+        return EleveService.update(eleve, id);
+    },
+
+    deleteEleve: (id) => {
+        return EleveService.delete(id);
+    },
+}
+
+module.exports = EleveController;
